2e198bc5bf30b02511c5cf6b49ebed70a1dcfbb3
tomasgz7 <tomasguzmancabj@hotmail.com>
2025-10-14 14:29:33 -0300

    portfolio project

---
+++ src/data/projects.ts
@@ -0,0 +1,23 @@
+export const projects = [
+  {
+    name: 'Resilience Media Group',
+    description: 'Web corporativa optimizada con foco en performance.',
+    stack: ['React', 'CSS3'],
+    github: 'https://github.com/tomasgz7',
+    demo: ''
+  },
+  {
+    name: 'Kronner Logistics',
+    description: 'App de seguimiento de envíos (Node + JS).',
+    stack: ['Node', 'JavaScript'],
+    github: 'https://github.com/tomasgz7',
+    demo: ''
+  },
+  {
+    name: 'Podcast Channel',
+    description: 'Sitio estático accesible con HTML + JS + CSS3.',
+    stack: ['HTML', 'CSS', 'JavaScript'],
+    github: 'https://github.com/tomasgz7',
+    demo: ''
+  }
+] as const;
